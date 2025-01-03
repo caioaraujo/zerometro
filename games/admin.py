c80f0ea6cbcd7b1061da870293a8d3fa24e81018
Caio Felipe de Araujo <c.felipe.araujo@gmail.com>
2025-01-03 13:57:53 -0300

    fix admin panel

--- games/admin.py
+++ games/admin.py
@@ -1,27 +1,35 @@
 from django.contrib import admin
 
-from .models import Game, Plataforma
+from .models import Game, Plataforma, Progresso
 
 
 class MyAdminSite(admin.AdminSite):
     site_header = "Zerometro administration"
 
 
+class ProgressoAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "game",
+        "finalizado",
+        "completado",
+        "lista_desejos"
+    ]
+    list_filter = [
+        "user"
+    ]
+    search_fields = ["game", "user"]
+
+
 class GameAdmin(admin.ModelAdmin):
     list_display = [
         "nome",
         "plataforma",
-        "finalizado",
-        "completado",
-        "lista_desejos",
         "midia",
     ]
     list_filter = [
         "nome",
         "plataforma",
-        "finalizado",
-        "completado",
-        "lista_desejos",
         "midia",
     ]
     search_fields = ["nome"]
@@ -36,3 +44,4 @@ class PlataformaAdmin(admin.ModelAdmin):
 admin_site = MyAdminSite(name="admin")
 admin_site.register(Game, GameAdmin)
 admin_site.register(Plataforma, PlataformaAdmin)
+admin_site.register(Progresso, ProgressoAdmin)
